16f695fc5a828d2a20a811c9b1b7165d467b3a77
SoulStyx <mattdasilva23@hotmail.co.uk>
2021-06-22 01:33:12 +0100

    second push

---
+++ README.md
@@ -0,0 +1,14 @@
+
+To run the Bitcoin app:
+
+1 - Download repository from GitHub
+
+2 - Navigate to 'dist' folder
+
+3 - Open bitcoin.bat
+
+A console window will open and you will then be asked to enter a 'duration' and 'interval' and the program will run.
+
+There is also a bitcoin.py file in the repository - this is just for viewing the source code.
+
+No building required.
--- bitcoin.py
+++ bitcoin.py
@@ -7,12 +7,12 @@ from prettytable import PrettyTable as pt
 # e.g. if param 'c' is USD, searches the json content/dictionary for USD and returns that value
 # the html.unescape converts the html entities within the json data to symbols, e.g. "&#36;" is the code for "$"
 def getBTCPrice(c, d):
-	return html.unescape(d["bpi"][c]["symbol"]) + str(round(d["bpi"][c]["rate_float"], 1))
+	return html.unescape(d["bpi"][c]["symbol"]) + str(format(round(d["bpi"][c]["rate_float"], 3), '.3f'))
 
 # quit program
 def quitProgram():
 	print("Quitting Program...")
-	os._exit(0)
+	os._exit(0) 
 
 # print prices
 def printPrices():
@@ -25,21 +25,21 @@ def printPrices():
 	content = r.json()
 
 	# the "content" variable is a dictionary, so we can index by using content["x"], e.g. for the time we use ["time"]["updateduk"] 
-	myTime = content["time"]["updateduk"]
+	# myTime = content["time"]["updateduk"]
 
 	# concise way of getting currencies without copy and paste
 	# this just so happens to work due to the way the json file is loaded, would be different in other situations
-	currencies = ["USD", "GBP", "EUR"]		# add or remove fields here to determine what prices to print
+	currencies = ["USD", "GBP", "EUR"]			# add or remove fields here to determine what prices to print
 	prices = {}
 	for curr in currencies:
 		prices["Bitcoin Price " + curr] = getBTCPrice(curr, content)
 
 	# PrettyTable is used to print all values nicely (vertically)
-	t = pt(["Title", "Value"])				# create table with field values
-	t.title = "Bitcoin Table"				# name of the table (will be at the top)
-	t.add_row(["Time Requested", myTime])	# add row - time requested
+	t = pt(["Currency", "Value"])				# create table with field values
+	t.title = "Bitcoin Table"					# name of the table (will be at the top)
+	# t.add_row(["Time Requested", myTime])		# add row - time requested (optional)
 	for k, v in prices.items():
-		t.add_row([k, v])					# add row - Bitcoin price, based on the amount of values in 'currencies' dictionary
+		t.add_row([k, v])						# add row - Bitcoin price, based on the amount of values in 'currencies' dictionary
 	print(t)
 
 # start of program
@@ -56,8 +56,9 @@ printPrices()										# call printPrices immediately (0 seconds), then do inter
 # ------------------------------------
 
 # old way of printing table (horizontally)
-# t = pt(["Time Requested", "Bitcoin Price USD", "Bitcoin Price GBP", "Bitcoin Price EUR"])	# headers of the table
-# t.add_row([myTime, btcUSD, btcGBP, btcEUR])	# values
+# t2 = pt(["Bitcoin Price USD", "Bitcoin Price GBP", "Bitcoin Price EUR"])									# headers of the table
+# t2.add_row([prices["Bitcoin Price USD"], prices["Bitcoin Price GBP"], prices["Bitcoin Price EUR"]])		# values
+# print(t2)
 
 # old code for reference
 # btcUSD = html.unescape(content["bpi"]["USD"]["symbol"]) + str(round(content["bpi"]["USD"]["rate_float"], 2))	# technically could just type "$" for USD, but better coding practice to use data
